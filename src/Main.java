5bf3a7b87d77a47f31e5fb8298e041a7b2df8d82
elwin24 <iamelvinnajafov24@gmail.com>
2022-10-09 22:09:50 +0400

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello football!");
+        List<Team> teams = new ArrayList<>();
+
+        Team team = new Team("Kapaz",12);
+        teams.add(team);
+        System.out.println(teams);
     }
 }
--- src/Team.java
+++ src/Team.java
@@ -1,2 +1,32 @@
 public class Team {
+    private String name;
+    private int point;
+
+    @Override
+    public String toString() {
+        return  "name='" + name + '\'' +
+                ", point=" + point +
+                '}';
+    }
+
+    public Team(String name, int point) {
+        this.name = name;
+        this.point = point;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
 }
